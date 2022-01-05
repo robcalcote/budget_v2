6b49136abf6047f646dc3ffa2e83e51fb8748cac
Rob Calcote <robertc@qualtrics.com>
2022-01-05 00:44:29 -0700

    copied routes from budget_v1. added user routes

---
+++ hello.py
@@ -0,0 +1,48 @@
+from flask import Flask
+# escapemarkup is broken... not really sure why, I think it might be a Q thing.
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return 'Home Page'
+
+@app.route('/transactions')
+def transactions():
+	# need to include endpoints for GET
+    return f'All Transactions'
+
+@app.route('/transaction/<uuid:id>')
+def transaction(id):
+	# need to include endpoints for GET, POST, PUT, DELETE
+    return f'Transaction id #{id}'
+
+@app.route('/categories')
+def categories():
+	# need to include endpoints for GET
+    return f'All Categories'
+
+@app.route('/category/<int:id>')
+def category(id):
+	# need to include endpoints for GET, POST, PUT, DELETE
+    return f'Category id #{id}'
+
+@app.route('/months')
+def months():
+	# need to include endpoints for GET
+    return f'All Months'
+
+@app.route('/month/<int:id>')
+def month(id):
+	# need to include endpoints for GET, POST, PUT, DELETE
+    return f'Month # {id}'
+
+@app.route('/users')
+def users():
+	# need to include endpoints for GET
+	return f'All Users'
+
+@app.route('/user/<int:id>')
+def user(id):
+	# need to include endpoints for GET, POST, PUT, DELETE
+	return f'User # {id}'
