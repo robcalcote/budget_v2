30e3d463f07766bbe4d31217a858731dd9aca46b
Rob Calcote <robertc@qualtrics.com>
2022-01-06 00:21:06 -0700

    read about method specification in routes, and the request object. Also read about templating.

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask import url_for
+from flask import render_template
+from flask import request
+
 
 app = Flask(__name__)
 
@@ -45,3 +49,20 @@ def users():
 def user(id):
 	# need to include endpoints for GET, POST, PUT, DELETE
 	return f'User # {id}'
+
+# as you can see, you can reference the function and get the name of the url route
+with app.test_request_context():
+    print(url_for('transactions'))
+    print(url_for('categories'))
+    print(url_for('category', id=456))
+    print(url_for('months'))
+    print(url_for('month', id=789))
+    print(url_for('users'))
+    print(url_for('user', id=100))
+    print(url_for('transactions', next='/'))
+
+# this is how you render a template
+@app.route('/hello/')
+@app.route('/hello/<string:name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
