e7540fd1d6e73e6d1540485ad3a40d7fcdd7b541
Rob Calcote <robertc@qualtrics.com>
2022-01-09 02:07:49 -0700

    tutorial - 90% through Templates
    removed old hello files from quick setup
    restructured project file structure
    sql commands to create the tables in sqlite
    Register, Login Templates
    Moved existing routes to __init__.py
    Backend DB queries for authentication
    SUPER basic template inheritance

---
+++ budget/__init__.py
@@ -0,0 +1,73 @@
+import os
+
+from flask import Flask
+
+from . import db
+from . import auth
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'budget.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    @app.route("/")
+    def index():
+        return 'Home Page'
+
+    @app.route('/transactions', methods=['GET'])
+    def transactions():
+        return f'All Transactions'
+
+    @app.route('/transaction/<uuid:id>', methods=['POST', 'PUT', 'GET', 'DELETE'])
+    def transaction(id):
+        return f'Transaction id #{id}'
+
+    @app.route('/categories', methods=['GET'])
+    def categories():
+        return f'All Categories'
+
+    @app.route('/category/<int:id>', methods=['POST', 'PUT', 'GET', 'DELETE'])
+    def category(id):
+        return f'Category id #{id}'
+
+    @app.route('/months', methods=['GET'])
+    def months():
+        return f'All Months'
+
+    @app.route('/month/<int:id>', methods=['POST', 'PUT', 'GET', 'DELETE'])
+    def month(id):
+        return f'Month # {id}'
+
+    @app.route('/users', methods=['GET'])
+    def users():
+        return f'All Users'
+
+    @app.route('/user/<int:id>', methods=['POST', 'PUT', 'GET', 'DELETE'])
+    def user(id):
+        return f'User # {id}'
+
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
+
+    return app
