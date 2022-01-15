bbe940834258fe8b3979d7da52ab61827bb00685
Rob Calcote <robertc@qualtrics.com>
2022-01-15 11:18:53 -0700

    dead code test ripout

--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -26,21 +26,6 @@ def test_login_required(client, path):
     assert response.headers['Location'] == 'http://localhost/auth/login'
 
 
-def test_author_required(app, client, auth):
-    # change the post author to another user
-    with app.app_context():
-        db = get_db()
-        db.execute('UPDATE post SET author_id = 2 WHERE id = 1')
-        db.commit()
-
-    auth.login()
-    # current user can't modify other user's post
-    assert client.post('/1/update').status_code == 403
-    assert client.post('/1/delete').status_code == 403
-    # current user doesn't see edit link
-    assert b'href="/1/update"' not in client.get('/').data
-
-
 @pytest.mark.parametrize('path', (
     '/2/update',
     '/2/delete',
