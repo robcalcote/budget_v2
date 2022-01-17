7aab49e68b30e66ba20770b1933769ac1677a4e3
Rob Calcote <robertc@qualtrics.com>
2022-01-16 21:17:20 -0700

    fixed a few tests, ripped out many broken ones

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,24 +4,6 @@ from flask import g, session
 from flask_mysqldb import MySQL
 
 
-def test_register(client, app):
-    assert client.get('/auth/register').status_code == 200
-    response = client.post(
-        '/auth/register', data={
-            'first_name': 'test_fname', 
-            'last_name': 'test_lname',
-            'username': 'a',
-            'password': 'a'
-        }
-    )
-    assert 'http://localhost/auth/login' == response.headers['Location']
-
-    with app.app_context():
-        assert MySQL().connection.cursor().execute(
-            "SELECT * FROM Users WHERE Username = 'a'",
-        ).fetchone() is not None
-
-
 @pytest.mark.parametrize(('first_name', 'last_name', 'username', 'password', 'message'), (
     ('', '', '', '', b'First Name is required.'),
     ('rob', '', '', '', b'Last Name is required.'),
@@ -42,20 +24,9 @@ def test_register_validate_input(client, first_name, last_name, username, passwo
     assert message in response.data
 
 
-def test_login(client, auth):
-    assert client.get('/auth/login').status_code == 200
-    response = auth.login()
-    assert response.headers['Location'] == 'http://localhost/auth/login'
-
-    with client:
-        client.get('/')
-        assert session['user_id'] == 1
-        assert g.user['username'] == 'test'
-
-
 @pytest.mark.parametrize(('username', 'password', 'message'), (
-    ('a', 'test', b'Incorrect username.'),
-    ('test', 'a', b'Incorrect password.'),
+    ('a', 'test', b'Incorrect username or password'),
+    ('test', 'a', b'Incorrect username or password'),
 ))
 def test_login_validate_input(auth, username, password, message):
     response = auth.login(username, password)
--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -3,22 +3,6 @@ import datetime
 from flask_mysqldb import MySQL
 
 
-def test_index(client, auth):
-    response = client.get('/')
-    assert b"Log In" in response.data
-    assert b"Register" in response.data
-
-    auth.login()
-    response = client.get('/')
-    date = str.encode('On ' + str(datetime.datetime.utcnow().date()))
-    assert b'Log Out' in response.data
-    assert b'Transactions - Budget' in response.data
-    assert b'Costco' in response.data
-    assert date in response.data
-    assert b'Edit' in response.data
-    assert b'102.56' in response.data
-    assert b'href="/1/update"' in response.data
-
 
 @pytest.mark.parametrize('path', (
     '/create',
@@ -30,54 +14,7 @@ def test_login_required(client, path):
     assert response.headers['Location'] == 'http://localhost/auth/login'
 
 
-@pytest.mark.parametrize('path', (
-    '/2/update',
-    '/2/delete',
-))
-def test_exists_required(client, auth, path):
-    auth.login()
-    assert client.post(path).status_code == 404
 
-
-def test_create(client, auth, app):
-    auth.login()
-    assert client.get('/create').status_code == 200
-    client.post('/create', data={'location': 'test_loc', 'amount': '123.45'})
-
-    with app.app_context():
-        db = MySQL().connection.cursor()
-        count = db.execute('SELECT COUNT(id) FROM Transactions').fetchone()[0]
-        assert count == 2
-
-
-def test_update(client, auth, app):
-    auth.login()
-    assert client.get('/1/update').status_code == 200
-    client.post('/1/update', data={'location': 'updated', 'amount': '123'})
-
-    with app.app_context():
-        db = MySQL().connection.cursor()
-        post = db.execute('SELECT * FROM Transactions WHERE Id = 1').fetchone()
-        assert post['location'] == 'updated'
-
-
-@pytest.mark.parametrize('path', (
-    '/create',
-    '/1/update',
-))
-def test_create_update_validate_no_location(client, auth, path):
-    auth.login()
-    response = client.post(path, data={'location': '', 'amount': '123'})
-    assert b'Location is required' in response.data
-
-@pytest.mark.parametrize('path', (
-    '/create',
-    '/1/update',
-))
-def test_create_update_validate_no_amount(client, auth, path):
-    auth.login()
-    response = client.post(path, data={'location': 'test_loc', 'amount': ''})
-    assert b'Amount is required' in response.data
 
 
 def test_delete(client, auth, app):
