18645bc06c115d6485acd8137043992a3866f56a
Rob Calcote <robertc@qualtrics.com>
2022-02-12 01:38:04 -0700

    reformat months on months page

--- budget/months.py
+++ budget/months.py
@@ -1,3 +1,4 @@
+from datetime import datetime as dt
 from flask import (
     Blueprint, request
 )
@@ -20,14 +21,19 @@ def get_month(id):
     return month
 
 def get_months():
-    db = get_db_connection()
-    curs = get_db_cursor(db)
-    curs.execute(
+	db = get_db_connection()
+	curs = get_db_cursor(db)
+	curs.execute(
 		f'SELECT m.Id, m.Month, m.Year, m.Savings, m.Projected, m.Actual' +
 		f' FROM Months m ORDER BY m.Year DESC, m.Month DESC;'
     )
-    months = curs.fetchall()
-    return months
+	months = curs.fetchall()
+	return months
+
+def reformat_months_month(months):
+	for m in months:
+		date = dt(m['Year'], m['Month'], 1)
+		m['Month'] = date.strftime('%B')
 
 def validate_months_fields(month=None, year=None, projected=None, actual=None, savings=None):
 	error = None
@@ -80,6 +86,7 @@ def get_one_month(id):
 @bp.route('/months', methods=(['GET']))
 def get_all_months():
 	m = get_months()
+	reformat_months_month(m)
 	res = {
 		'response': 'success',
 		'months': m
